52eb0d39cd70351c1ccd9f83ed91364107ea8a87
MichaelFan <fan122316@gmail.com>
2017-11-10 07:32:43 +0800

    p57

--- Pro_ObjectiveC/Pro_ObjectiveC/ViewController.m
+++ Pro_ObjectiveC/Pro_ObjectiveC/ViewController.m
@@ -20,6 +20,45 @@ extern void _objc_autoreleasePoolPrint();
     
     _objc_autoreleasePoolPrint();
     
+    NSObject *strongObj = [[NSObject alloc] init];
+    NSObject __weak *weakObj = strongObj;
+    NSLog(@"%@",weakObj);
+    
+    //必须指定__strong 默认是__autoreleasing。赋值给对象指针时 对象指针时 对象指针时 所有权修饰符必须一致。
+    NSObject * __strong *testPoint = &strongObj;
+    
+    /*
+     编译器自动做以下操作
+     NSObject __autoreleasing *tmp = strongObj;
+     [self testPoint:&tmp];
+     strongObj = tmp;
+     */
+    [self testPoint:&strongObj];
+    
+    @autoreleasepool {
+        
+    }
+    
+    struct data {
+        //对象加入c语言结构体struct/union中时 只能强转为void*或附加__unsafe_unretained修饰符
+        NSMutableArray __unsafe_unretained *array;
+    };
+    
+    id oc = [[NSObject alloc] init];
+    void *p = (__bridge_retained void *)oc;
+    oc = (__bridge_transfer id)p;
+    
+    
+}
+
+//两个方法参数等同 对象的指针默认__autoreleasing
+-(void)testPoint:(NSObject **)obj
+{
+    //因为默认是__autoreleasing 所以obj是一个注册到autoreleasepool中的对象
+    *obj = [[NSObject alloc] init];
+}
+-(void)testAutoReleasePoint:(NSObject * __autoreleasing *)error
+{
     
 }
 
