7db14cd2e416b4d92949954f754382d7318c9558
MichaelFan <fan122316@gmail.com>
2017-11-13 07:31:39 +0800

    test block

---
+++ Pro_ObjectiveC/Pro_ObjectiveC/test_block.c
@@ -0,0 +1,15 @@
+//
+//  test_block.c
+//  Pro_ObjectiveC
+//
+//  Created by MichaelFan on 2017/11/13.
+//  Copyright © 2017年 MichaelFan. All rights reserved.
+//
+
+void testBlock(){
+    
+    void(^blk)(void) = ^void(){ int abc = 1; };
+    
+}
+
+
