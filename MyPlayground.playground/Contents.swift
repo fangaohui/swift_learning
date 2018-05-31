f3a11cfdd259e16ad94f58ef4436647a69cc08f5
MichaelFan <fan122316@gmail.com>
2018-05-31 08:10:30 +0800

    add test

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -3,12 +3,13 @@
 import UIKit
 
 var str = "Hello, playground"
+str += "1"
 var test : Int = 1
 let tt : Int = test
 var str_test = "\(test) has"
 if test == tt {
-    test = Int.max &+ 10
-    var test1 = Int32.max
+    test = 19//Int.max &+ 10
+    var test1 = 19//Int32.max
 }
 switch test {
 case 1...100:
@@ -33,7 +34,76 @@ for i in 10...100 where i % 3 == 0 {
     test
     print(test)
 }
-while case 100...100 = test {
+while case 100...500 = test {
     print("123")
 }
+for scalar in str.unicodeScalars {
+    print("\(scalar.value)");
+}
+var errorCodeString:String?
+errorCodeString = "404aaa"
+var testErrorCodeString:String?
+print(testErrorCodeString)
+if let testTemporary = testErrorCodeString, var testInt = Int(testTemporary) {
+    print(testTemporary)
+}
+else
+{
+    print("123321")
+}
+var test111 = errorCodeString
+var test11122:String = errorCodeString!
+var hhaa:String! = nil
+var err = hhaa
+var testErrorCodeString111:String = "123ff"
+var upCaseError:String? = errorCodeString?.uppercased()
+var upCaseError112:String = testErrorCodeString111.uppercased()
+
+if let testErrorCodeString = testErrorCodeString {
+    upCaseError112 = testErrorCodeString
+    print(testErrorCodeString)
+}
+else
+{
+    print(testErrorCodeString)  //注意if中let testErrorCodeString的作用域
+    upCaseError112 = "eor"
+}
+upCaseError112 = testErrorCodeString ?? "eor"
+var bucketList = ["asd","dddd"]
+var newList = ["123111"]
+newList += bucketList
+newList.insert("hhh", at: 3)
+newList
+bucketList
+var firstDic = Dictionary<String,String>()
+var secDic: Dictionary<String,String> = [:]
+var thirDic : [String:String] = [:]
+var forthDic = [String:String]()
+for move in forthDic.values {
+    
+}
+for keys in forthDic.keys {
+    
+}
+for (key,value) in forthDic {
+    
+}
+for keys in forthDic {
+    
+}
+let newAr = Array(forthDic.keys)
+let newAr11 = Array([123])
+newAr11
+var newset = Set([1233,333])
+var newset11 = Set([1233,333,44])
+newset.union(newset11)
+newset
+
+
+
+
+
+
+
+
 
